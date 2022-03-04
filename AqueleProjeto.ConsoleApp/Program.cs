ecd070ebe28e3a8a286e9d2e76383c6e3baa56dc
Vogeel <vitorvogel12@gmail.com>
2022-03-04 17:30:28 -0300

    a

--- AqueleProjeto.ConsoleApp/Program.cs
+++ AqueleProjeto.ConsoleApp/Program.cs
@@ -362,7 +362,7 @@ namespace AqueleProjeto.ConsoleApp
 
                                    Console.ReadLine();
                                    Console.Clear();
-                                   break;
+                                   goto menu;
                             }
 
                         }
